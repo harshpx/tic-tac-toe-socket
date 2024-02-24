e85f21a3fd35b2d17eaefeea8bfe28177f69e673
harshpx <harsh.rzf@gmail.com>
2024-02-24 05:39:56 +0530

    corrected socket endpoint

--- client/src/features/socket.js
+++ client/src/features/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 
-const SOCKET_ENDPOINT = "http://localhost:5000/";
+const SOCKET_ENDPOINT = "https://tic-tac-toe-socket-api-9f03754f47c8.herokuapp.com/";
 export const socket = io(SOCKET_ENDPOINT,{
     autoConnect: false
 });
