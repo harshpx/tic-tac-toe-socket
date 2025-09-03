9b80a5594c5ec8e845cb0b278c330be09849eeac
harshpx <harsh.rzf@gmail.com>
2025-09-04 02:20:31 +0530

    updated server url in client

--- client/src/features/socket.js
+++ client/src/features/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_ENDPOINT = "http://localhost:5002";
+const SOCKET_ENDPOINT = "https://ttt.174.138.122.195.nip.io";
 export const socket = io(SOCKET_ENDPOINT, {
   autoConnect: false,
 });
